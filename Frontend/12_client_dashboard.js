8e501ef701983b3b8b1c3972132733ff8965c3d2
Givenyash <yash.ishu.barehi@gmail.com>
2026-07-19 16:47:11 +0530

    Client Dashboard (HTML5, CSS3, JavaScript).

---
+++ Frontend/12_client_dashboard.js
@@ -0,0 +1,23 @@
+const name = localStorage.getItem("userName");
+
+if(name){
+
+    document.getElementById("clientName").innerHTML = name;
+
+}
+
+function logout(){
+
+    localStorage.clear();
+
+    window.location.href = "2_sign_in.html";
+
+}
+
+function becomeSeller(){
+
+    localStorage.setItem("role","seller");
+
+    window.location.href = "6_profile_overview.html";
+
+}
